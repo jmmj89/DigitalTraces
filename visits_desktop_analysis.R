4c32d5618b475a59fa3aa5b5f826ca719815ba64
Annalilla <harman.anna@gmail.com>
2020-05-21 15:14:50 +0200

    Update visits_desktop_analysis.R

--- visits_desktop_analysis.R
+++ visits_desktop_analysis.R
@@ -13,7 +13,7 @@ source("ISCED_coding.R")
 source("data_prep.R")
 source("visits_prob.R")
 
-#all_cat <- read.csv("predicted_cats/Number_of_visits_all_Category.csv")
+all_cat <- read.csv("predicted_cats/Number_of_visits_all_Category.csv")
 
 head(all_cat)
 
@@ -77,8 +77,9 @@ table(all_cat_p$cluster)
 full_dat <- merge(all_cat_p, exp_dat, by = "panelist_id", all.x = TRUE)
 full_dat[,c("cluster", "undecided", "polinterest")] <- as.data.frame(apply(full_dat[,c("cluster", "undecided", "polinterest")], 2, factor))
 full_dat[,which(sapply(full_dat, is.character))] <- as.data.frame(apply(full_dat[,which(sapply(full_dat, is.character))], 2, factor))
+str(full_dat$cluster)
 
-full_dat$cluster <- relevel(full_dat$cluster, ref = "1") # The reference category is routine.
+full_dat$cluster <- relevel(full_dat$cluster, ref = "4") # Set the reference cluster category to ‘unknown’ news consumption
 
 # Splitting the data per countries
 countries <- split(full_dat, full_dat$country)
@@ -87,12 +88,9 @@ UK <- countries$UK
 Germany <- countries$Germany
 
 
-#Diagnostics
-summary(full_dat)
-#1 checking missings: have missingsin sociodemographics, we should omit them in the multinom regr.
-colSums(is.na(full_dat)) 
+# Get result statistics and output tables
 
-log_reg_res <- function(reg_model) #Function to produce measures of the model and produce output tables:
+log_reg_res <- function(reg_model)
 {
   res <- list()
   
@@ -110,23 +108,23 @@ log_reg_res <- function(reg_model) #Function to produce measures of the model an
   hess <- reg_model$Hessian # Hessian observed/expected information matrix
   res[[5]] <- hess
   
-  cluster2 <- rbind(risk_ratio[1,], summary(reg_model)$coefficients[1,], summary(reg_model)$standard.errors[1,],z[1,],p[1,]) #Get a table 
-  #of coefficients, standard errors, z stats, and p values for cluster = 2 (cluster 1 'routine' is the reference category)
-  rownames(cluster2) <- c("Risk ratio", "Coefficient","Std. Errors","z stat","p value")
-  res[[6]] <- knitr::kable(cluster2)
+  cluster1 <- rbind(risk_ratio[1,], summary(reg_model)$coefficients[1,], summary(reg_model)$standard.errors[1,],z[1,],p[1,]) #Get a table
+  #of coefficients, standard errors, z stats, and p values for cluster = 1 (cluster 4 'unknown' is the reference category)
+  rownames(cluster1) <- c("Risk ratio", "Coefficient","Std. Errors","z stat","p value")
+  res[[6]] <- knitr::kable(cluster1)
   
-  cluster3 <- rbind(risk_ratio[2,],summary(reg_model)$coefficients[2,],summary(reg_model)$standard.errors[2,],z[2,],p[2,]) #Get a table 
+  cluster2 <- rbind(risk_ratio[2,],summary(reg_model)$coefficients[2,],summary(reg_model)$standard.errors[2,],z[2,],p[2,]) #Get a table
+  #of coefficients, standard errors, z stats, and p values for cluster = 2
+  rownames(cluster2) <- c("Risk ratio","Coefficient","Std. Errors","z stat","p value")
+  res[[7]] <- knitr::kable(cluster2)
+  
+  cluster3 <- rbind(risk_ratio[3,], summary(reg_model)$coefficients[3,],summary(reg_model)$standard.errors[3,],z[3,],p[3,]) #Get a table
   #of coefficients, standard errors, z stats, and p values for cluster = 3
-  rownames(cluster3) <- c("Risk ratio","Coefficient","Std. Errors","z stat","p value")
-  res[[7]] <- knitr::kable(cluster3)
-  
-  cluster4 <- rbind(risk_ratio[3,], summary(reg_model)$coefficients[3,],summary(reg_model)$standard.errors[3,],z[3,],p[3,]) #Get a table 
-  #of coefficients, standard errors, z stats, and p values for cluster = 4 
-  rownames(cluster4) <- c("Risk ratio", "Coefficient","Std. Errors","z stat","p value")
-  res[[8]] <- knitr::kable(cluster4)
+  rownames(cluster3) <- c("Risk ratio", "Coefficient","Std. Errors","z stat","p value")
+  res[[8]] <- knitr::kable(cluster3)
   
   
-  prob <-summary(reg_model)$fitted.values # We can also use probabilities to understand our model: 
+  prob <-summary(reg_model)$fitted.values # We can also use probabilities to understand our model:
   res[[9]] <- prob
   
   
@@ -136,20 +134,17 @@ log_reg_res <- function(reg_model) #Function to produce measures of the model an
   acc <- round((sum(diag(ctable))/sum(ctable))*100,2) # Calculating accuracy - sum of diagonal elements divided by total obs
   res[[10]] <- acc
   
-  names(res) <- c("summary", "z", "p", "risk_ratio", "Hessian", "cluster2", "cluster3", "cluster4", "prob", "acc")
+  names(res) <- c("summary", "z", "p", "risk_ratio", "Hessian", "cluster1", "cluster2", "cluster3", "prob", "acc")
   res
 }
 
-
 ### Political explanatory variables
-
-#Here are the variables I used. These should be used in the random forest as well to be able to compare the results.
 formula_pol <- as.formula(cluster ~ reg_vote + voted + change + undecided + polinterest.num + leftmidright.num +
                             trust.EP + trust.nat.pol)
 
 mod_pol <- multinom(formula_pol, data = full_dat,
                     na.action = na.omit, # omit missing observations
-                    Hess = TRUE) #get Hessian observed/expected information matrix
+                    Hess = TRUE) #get Hessian observed/expected information matrix)
 res_pol <- log_reg_res(mod_pol)
 res_pol
 
@@ -163,15 +158,14 @@ res_pol_France <- log_reg_res(mod_pol_France)
 res_pol_UK <- log_reg_res(mod_pol_UK)
 res_pol_Germany <- log_reg_res(mod_pol_Germany)
 res_pol_France
-res_pol_UK 
-  res_pol_Germany
+res_pol_UK
+res_pol_Germany
 
-### Political and sociodemographical explanatory variables together
-#Here are the variables I used. These should be used in the random forest as well to be able to compare the results.
+### Political and sociodemographical explanatory variables
 formula_pol_socio <- as.formula(cluster ~ reg_vote + voted + change + undecided + polinterest.num + leftmidright.num + trust.EP +
                                   trust.nat.pol + gender + age_num + children + income + family + ISCED)
 
-mod_pol_socio <- multinom(formula_pol_socio, data = full_dat, 
+mod_pol_socio <- multinom(formula_pol_socio, data = full_dat,
                           na.action = na.omit, # omit missing observations
                           Hess = TRUE) #get Hessian observed/expected information matrix
 res_pol_socio <- log_reg_res(mod_pol_socio)
@@ -186,12 +180,16 @@ res_pol_socio_France <- log_reg_res(mod_pol_socio_France)
 res_pol_socio_UK <- log_reg_res(mod_pol_socio_UK)
 res_pol_socio_Germany <- log_reg_res(mod_pol_socio_Germany)
 res_pol_socio_France
-res_pol_socio_UK 
+res_pol_socio_UK
 res_pol_socio_Germany
 
-## Model evaluation
+## Model description
+print(res_pol$summary$convergence) #0 means converged
+print(res_pol$summary$AIC) 
+print(res_pol$acc) #accuracy
 
-#function to get convergence, AIC and accuracy for evaluation table.
+
+#Prepare model description tables
 selection_res <- function(sel_crit)
 {
   sel_crit <- c(sel_crit$summary$convergence, sel_crit$summary$AIC, sel_crit$acc) #model selection criteria
@@ -215,11 +213,10 @@ pol_Germany <- as.data.frame(selection_res(res_pol_Germany))
 rownames(pol_Germany) <-"pol_Germany"
 
 
-#Model evaluation data for total data and per countries for the political variables model.
 pol <- rbind(total_pol, pol_France, pol_Germany, pol_UK)
 str(pol)
 
-#For politics and sociodemographics together.
+#For politics and sociodemographics
 total_pol_socio <- as.data.frame(selection_res(res_pol_socio))
 str(total_pol_socio)
 rownames(total_pol_socio) <-"pol_socio"
@@ -233,19 +230,23 @@ rownames(pol_socio_UK) <-"pol_socio_UK"
 pol_socio_Germany <- as.data.frame(selection_res(res_pol_socio_Germany))
 rownames(pol_socio_Germany) <-"pol_socio_Germany"
 
-#Model evaluation data for total data and per countries for the political and sociodemographic variables model.
+
 pol_socio <- rbind(total_pol_socio, pol_socio_France, pol_socio_Germany, pol_socio_UK)
 str(pol_socio)
+
 
 # Effect stars
 library(nnet)
 library(EffectStars2)
 library(graphics)
 
+dir.create("effectstars_extended", showWarnings = FALSE)
+
 #Function to prepare political effectstars
 
-effectstars_pol <- function(model)
+effectstars_pol <- function(model, file)
 {
+  filename <- paste("effectstars_extended/", file, ".png", sep = "")
   risk_ratio <- exp(coef(model)) #Get the Beta coefficients (risk ratios)   
   e_star_risk_ratio <- t(risk_ratio) #Transpose the risk ratio matrix in order to get the right format to feed into Effectstars2
   
@@ -259,6 +260,7 @@ effectstars_pol <- function(model)
                     cex.main = 1.5, cex.labels = 1.2, col.main = "black",
                     col.labels = "black", col.star = "black", dist.labels = 1.1, 
                     font.labels = 1, radius = 1)
+  png(filename = filename, width=800, height=600)
   effectstars(e_star_risk_ratio,
               names = c("Intercept", "Registered voters", "Voted", rep("Changed mind", 3), 
                         "Undecided", "Polinterest", "Leftmidright", "Trust in EP", "Trust in NP"), #name of the star
@@ -266,20 +268,22 @@ effectstars_pol <- function(model)
               #category labels of the predictors 
               labels = labels, #dependent variable categories
               control = ctrl) #graphic above
+  dev.off()
   
   
 }
 
 #Plot political effectstars
-effectstars_pol(mod_pol)
-effectstars_pol(mod_pol_France)
-effectstars_pol(mod_pol_Germany)
-effectstars_pol(mod_pol_UK)
+effectstars_pol(mod_pol, "effectstars_pol")
+effectstars_pol(mod_pol_France, "effectstars_pol_Fr")
+effectstars_pol(mod_pol_Germany, "effectstars_po_Ger")
+effectstars_pol(mod_pol_UK, "effectstars_UK")
 
 #Function to prepare political and sociodemographic effectstars
 
-effectstars_pol_socio <- function(model)
+effectstars_pol_socio <- function(model, file)
 {
+  filename <- paste("effectstars_extended/", file, ".png", sep = "")
   risk_ratio <- exp(coef(model)) #Get the Beta coefficients (risk ratios)   
   e_star_risk_ratio <- t(risk_ratio) #Transpose the risk ratio matrix in order to get the right format to feed into Effectstars2
   
@@ -293,7 +297,7 @@ effectstars_pol_socio <- function(model)
                     cex.main = 1, cex.labels = 1, col.main = "black",
                     col.labels = "black", col.star = "black", dist.labels = 1.1, 
                     font.labels = 1, radius = 1)
-  
+  png(filename = filename, width=800, height=600)
   effectstars(e_star_risk_ratio,
               names = c("Intercept",   #name of the star 
                         "Registered voters", 
@@ -318,12 +322,12 @@ effectstars_pol_socio <- function(model)
               labels = labels, #dependent variable categories
               
               control = ctrl) #graphic above
-  
+  dev.off()
 }
 
 
 #Plot political and sociodemographic effectstars
-effectstars_pol_socio(mod_pol_socio)
-effectstars_pol_socio(mod_pol_socio_France)
-effectstars_pol_socio(mod_pol_socio_Germany)
-effectstars_pol_socio(mod_pol_socio_UK)
+effectstars_pol_socio(mod_pol_socio, "effectstars_pol_socio")
+effectstars_pol_socio(mod_pol_socio_France, "effectstars_pol_socio_Fr")
+effectstars_pol_socio(mod_pol_socio_Germany, "effectstars_pol_socio_Ger")
+effectstars_pol_socio(mod_pol_socio_UK, "effectstars_pol_socio_UK")
